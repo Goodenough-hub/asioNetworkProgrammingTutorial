ca2d00da0ba84ae532f469c657239138193a5de3
Goodenough <2323342501@qq.com>
2024-10-30 15:59:02 +0800

    upload boosttest、endpoint

---
+++ endpoint/endpoint.h
@@ -0,0 +1,9 @@
+#pragma once
+extern int client_end_point(); // 客户端——创建端点
+extern int server_end_point(); // 服务器端——创建端点
+extern int create_tcp_socket(); // 客户端——创建socket
+extern int create_acceptor_socket(); // 服务器端——创建socket
+extern int bind_acceptor_socket(); // 服务器端——绑定本机ip+port
+extern int connect_to_end(); // 客户端——连接操作
+extern int dns_connect_to_end(); // 客户端——解析域名，再实现从客户端到服务器的连接
+extern int accept_new_connection(); // 服务器端——接受一个连接
